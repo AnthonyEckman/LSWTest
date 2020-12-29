8b5fd1fd009e251f6644f8880fb156227a90acaa
DESKTOP-6HTUSTO\scout <anthonyeckman@gmail.com>
2020-12-28 19:00:34 -0600

    Inventory Manager

--- TSWTest/Assets/Scripts/InventoryManager.cs
+++ TSWTest/Assets/Scripts/InventoryManager.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public class InventoryManager : MonoBehaviour
 {
+    //singleton class that manages all player invetory interactions
 
     private static InventoryManager _instance;
     
     public static InventoryManager Instance { get { return _instance; } }
 
+    //current inventory player is using
+    public PlayerInventory CurrentInventory;
 
-
+    //default inventory that is defaulted to on game start.
+    public PlayerInventory DefaultInventory;
 
     private void Awake()
     {
@@ -24,9 +28,41 @@ public class InventoryManager : MonoBehaviour
         }
     }
 
+    private void Start()
+    {
+        ResetInventory();
+    }
+
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void ResetInventory()
+    {
+        CurrentInventory.Money = DefaultInventory.Money;
+        CurrentInventory.PlayerItems = DefaultInventory.PlayerItems;
+    }
+
+
+    public void AddToPlayerInventory(BaseItem newItem)
+    {
+        CurrentInventory.PlayerItems.Add(newItem);
+    }   
+
+    public void RemoveFromPlayerInventroy(BaseItem item)
+    {
+        CurrentInventory.PlayerItems.Remove(item);
+    }
+
+    public void AddMoney(int ammount)
+    {
+        CurrentInventory.Money += ammount;
+    }
+
+    public void RemoveMoney(int ammount)
+    {
+        CurrentInventory.Money -= ammount;
     }
 }
